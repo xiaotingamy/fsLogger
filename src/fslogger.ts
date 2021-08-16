fa7fed8babf1a894fdbe97157b138793a1c7ea10
guoxiaoting <guoxiaoting@fshows.com>
2021-08-16 11:54:28 +0800

    feat:

--- src/fslogger.ts
+++ src/fslogger.ts
@@ -1,6 +1,8 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+// import "core-js/fn/array.find"
+// ...
 export default class DummyClass {
-
+  log() {
+    console.log('333')
+  }
 }
